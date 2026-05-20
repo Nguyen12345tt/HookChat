827ccc15cf3d9032d32a2d621ddb5f5db3194210
Nguyen12345tt <thanhnguyennguyen496@gmail.com>
2026-05-20 17:24:30 +0700

    fix: update next.config.mjs to include transpilePackages for dependencies

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  transpilePackages: ['emoji-picker-react', 'socket.io-client', 'engine.io-client'],
 };
 
 export default nextConfig;
